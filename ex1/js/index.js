f80a7d88d14d3f2d815cdce8e52e5a1db54f659f
kitriaa <frozenfireblast@gmail.com>
2016-07-29 21:28:09 -0700

    web assignment

--- ex1/js/index.js
+++ ex1/js/index.js
@@ -3,4 +3,11 @@ window.onload = function () {
   // Make it listen for a Click event
     // this event should log to the console the text inside of the <a> tag
     // the event should only log one word per click.
+    var events1 = document.getElementsByClassName("navLink");
+    for (i = 0; i < events1.length; i++) {
+    	events1[i].addEventListener("click", print);
+    }
+function print() {
+	console.log(this.textContent);
+}
 };
--- ex2/js/index.js
+++ ex2/js/index.js
@@ -3,4 +3,16 @@ window.onload = function () {
   // inspect the .css file
     // when a `child` class is clicked you code will add CSS styles which will show the
     // secret toy element. Click on an element will only show that element's toy, no one elses.
-};
+    var elemental = document.getElementsByClassName("child");
+    for (i = 0; i < elemental.length; i++) {
+    	elemental[i].addEventListener("click", print);
+    }
+    function print() {
+    	var toy = this.querySelectorAll("p.secret-toy")[0];
+    	if(toy.style.display === "none") {
+    		toy.style.display = "inline";
+    	} else {
+    		toy.style.display = "none";
+    	}
+    }
+};  
